84ef06ea20d1c32457383d3f75162ed736fbcd44
Nikolay <ledengskiy@gmail.com>
2019-12-21 21:31:40 +0700

    deleted old code, added getting words from db , trying to make adding to own words list

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
-    <p>{{isVis}}</p>
-    <p>{{added}}</p>
-    <ul>
-      <li v-for="word of words">{{word.value}} : {{word.trans}} <button @click="addWord(word)">добавить</button></li>
-    </ul>
-    
-    <p>Проверить слова : <button @click="isVis=!isVis">поехали</button></p>
-    <ul v-show="isVis">
-      <li v-for="(word , index) of added">{{index}}{{word.value}}: <input  v-model="userTranslets[index]" @input="check(index)"  type="text">{{userTrans[index]}} </li>
-    </ul>
-    <div>
-      <label for="trans">добавить слово в БД</label>
-      <input type="text" v-model="newWord">
+    <div class="newWordForm">
+          <div>
+            <label for="trans">добавить слово в БД</label>
+            <input type="text" v-model="newWord">
+          </div>
+          <div>
+            <label for="word">добавить его перевод в БД</label>
+            <input type="text" v-model="newTrans">
+          </div>
+              <button @click="createWord">Создать слово</button>
     </div>
-    <div>
-      <label for="word">добавить его перевод в БД</label>
-      <input type="text" v-model="newTrans">
+    <div class="vacabulary">
+      <button @click="getVacabulary">Загрузить словарь</button>
+      <ul>
+        <li 
+        v-for="word in words"
+        :key="word.id"
+        > <strong>{{word.value}}</strong> : {{word.trans}}
+        <button @click="addToOwnVac(word)">+++</button>
+        </li>
+      </ul>
+
     </div>
-    <button @click="createWord">Создать слово</button>
+
   </div>
 </template>
 
@@ -27,20 +32,9 @@
 export default {
   data () {
     return {
-      msg: 'Привет',
-      words: [
-        {value:'red', trans:'красный'},
-        {value:'blue', trans:'синий'},
-        {value:'green', trans:'зеленый'}
-      ],
-      newWord : '',
-      newTrans : '',
-      added : [],
-      isAdded : false,
-      userTranslets : [],
-      userTrans : [],
-      isVis: false,
-      transl : ''   
+            newWord : '',
+            newTrans : '',
+            words : []
     }
   },
   methods:{
@@ -49,27 +43,37 @@ export default {
         value: this.newWord,
         trans: this.newTrans
       }
+      console.log(nWord)
       this.$http.post('http://localhost:3000/words',nWord)
       .then(response =>{
-        console.log(response)
+        return response.json()
+      })
+      .then(getWord =>{
+        console.log(getWord)
       })
     },
-    addWord (word) {
-      let first=this.isAdded
-      for(let i=0;i<this.added.length;i++){
-        if(word==this.added[i]){
-          this.isAdded=!first
-          }
-      }
-      if(this.isAdded==first){this.added.push(word)}
+    getVacabulary (){
+      this.$http.get('http://localhost:3000/words')
+      .then(response =>{
+        return response.json()
+      })
+      .then(getAllWords =>{
+        this.words = getAllWords
+      })
     },
-    check (index) {
-      if(this.userTranslets[index]==this.added[index].trans){
-        this.userTrans[index]=true
+    addToOwnVac : (word) =>{
+      const addedWord = {
+        value : word.value,
+        trans : word.trans
       }
-      else{
-        this.userTrans[index]=false
-      }
+      console.log(addedWord)
+      this.$http.post('http://localhost:3000/userVacabulary',addedWord)
+      .then(response =>{
+        return response.json()
+      })
+      .then(getWord =>{
+        console.log(getWord)
+      })
     }
   }
 }
